557e06d0b172eae796096ae15c96f17ef67c9da1
Matt Mower <mowerm@gmail.com>
2017-01-19 14:22:47 -0600

    s4-common: Add minimal init.qcom.fm.sh
    Directly setprop'ing via service is being denied by selinux. We still
    don't need the full fledged init.qcom.fm.sh since we neither use
    kernel modules nor provide fm_qsoc_patches, so minify the script to
    perform the setprop we desire and be done.
    
    Change-Id: If607a36e9a3b6dcb3cfdfc69e416df833fc0f1e0

--- s4.mk
+++ s4.mk
@@ -50,7 +50,8 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
+    $(LOCAL_PATH)/configs/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+    $(LOCAL_PATH)/configs/init.qcom.fm.sh:system/etc/init.qcom.fm.sh
 
 # Shims
 PRODUCT_PACKAGES += \
